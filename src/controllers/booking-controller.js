d1b7af8ff170aa92ead8363fd5544c1ef6a677b6
Esha <maheshwariesha11@gmail.com>
2024-01-22 16:19:55 +0530

    message queue continued

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -5,7 +5,7 @@ const { BookingService } = require('../services/index');
 const { createChannel, publishMessage } = require('../utils/messageQueue');
 const { REMINDER_BINDING_KEY } = require('../config/serverConfig')
 
-const bookingService = new BookingService();
+const bookingService = new BookingService();    
 
 class BookingController {
     
@@ -14,8 +14,16 @@ class BookingController {
 
     async sendMessageToQueue(req, res) {
         const channel = await createChannel();
-        const data = {message: 'Success'};
-        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        const payload = {
+           data: {
+                subject: 'This is a noti from queue',
+                content: 'Some queue will subscribe this',
+                recipientEmail: 'eshadhoot20@gmail.com',
+                notificationTime: '2024-01-22T12:49:00'   
+           },
+           service: 'CREATE_TICKET'
+        }; 
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload ));
         return res.status(StatusCodes.OK).json({
             message: 'Successfully published the event'
         });
